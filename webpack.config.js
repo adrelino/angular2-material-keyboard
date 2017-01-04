2107867767723b2ade21471e945b3e7478699e69
Claudiu Teodor Filip <cfilip@opswat.com>
2017-01-04 15:29:56 +0200

    fixed build config and output

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
 var path = require('path');
 
 module.exports = {
-  entry: "./src/index.ts",
-  output: {
-    filename: "index.js",
-    umdNamedDefine: true,
-    library: 'angular2-material-keyboard',
-    libraryTarget: 'umd'
-  },
-  devtool: 'source-map',
+    entry: "./src/index.ts",
+    output: {
+        path: "./dist",
+        filename: "index.js",
+        umdNamedDefine: true,
+        library: 'angular2-material-keyboard',
+        libraryTarget: 'umd'
+    },
+    devtool: 'source-map',
 
-  resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js'],
-    // Two lines below fix dependency resolution when npm linking the component.
-    fallback: path.join(__dirname, "node_modules")
-  },
-  resolveLoader: {fallback: path.join(__dirname, "node_modules")},
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js'],
+        // Two lines below fix dependency resolution when npm linking the component.
+        fallback: path.join(__dirname, "node_modules")
+    },
+    resolveLoader: {fallback: path.join(__dirname, "node_modules")},
 
-  module: {
-    loaders: [
-      {
-        test: /\.ts$/, loader: 'ts-loader'
-      }
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/, loader: 'ts-loader'
+            }
+        ]
+    },
+    noParse: [
+        path.join(__dirname, 'node_modules', 'zone.js', 'dist')
     ]
-  },
-  noParse: [
-    path.join(__dirname, 'node_modules', 'zone.js', 'dist')
-  ]
 };
